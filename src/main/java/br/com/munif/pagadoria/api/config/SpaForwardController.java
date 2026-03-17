26e2cc2847bde28f6a262f62765802aba9ac20aa
munif <munifgebara@gmail.com>
2026-03-17 12:54:13 -0300

    Início do FrontEnd com Angular e ajustes no spring e configurações.

--- src/main/java/br/com/munif/pagadoria/api/config/SpaForwardController.java
+++ src/main/java/br/com/munif/pagadoria/api/config/SpaForwardController.java
@@ -1,4 +1,18 @@
 package br.com.munif.pagadoria.api.config;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SpaForwardController {
+
+    @GetMapping({
+            "/app",
+            "/app/",
+            "/app/{path:[^\\.]*}",
+            "/app/**/{path:[^\\.]*}"
+    })
+    public String forward() {
+        return "forward:/app/index.html";
+    }
 }
